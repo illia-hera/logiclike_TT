a04f3a5c5ae7b9f12d536660e779ae47e910c6f4
IlyaG <ilyagerasimchik@gmail.com>
2024-04-26 19:52:29 +0300

    refactor: removed router

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import Layout from "./pages/Layout/index";
-import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Layout />
-        </BrowserRouter>
+        <Layout />
     );
 }
 
