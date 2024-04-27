294d47d85e7cc1c117ae1789c89311e1f24eba16
IlyaG <ilyagerasimchik@gmail.com>
2024-04-27 11:08:20 +0300

    feat: added card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import style from "./index.module.scss"
+import ICard from "types/ICard";
+
+const Card = ({ id, image, name, bgColor }: ICard) => {
+    const [imageStyle, setImageStyle] = useState({})
+
+    useEffect(() => {
+        setImageStyle({
+            backgroundColor: bgColor
+        })
+    }, []);
+
+    return (
+        <div key={id} className={style.card}>
+            <div className={style.image} style={imageStyle}>
+                <img src={image} />
+            </div>
+            <div className={style.title}>{name}</div>
+        </div>
+    );
+}
+
+export default Card;
