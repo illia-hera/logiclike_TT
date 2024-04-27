336f715bae97b4b44ce7122218388b728ffe9321
IlyaG <ilyagerasimchik@gmail.com>
2024-04-27 11:10:23 +0300

    feat: added content component

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import style from "./index.module.scss"
+import ICard from "types/ICard";
+import Card from "components/Card";
+
+interface IProps{
+    cards: ICard[]
+}
+
+const Content = ({cards}: IProps) => {
+    const renderCards = () => {
+        return cards.map((c, n) => <Card key={n} {...c}/>)
+    }
+
+    return (
+        <div className={style.content}>
+            {renderCards()}
+        </div>
+    );
+}
+
+export default Content;
